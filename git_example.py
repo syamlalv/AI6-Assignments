5feecf700b8f5c61cb4f304ce1ce80b9d207771e
syamlalv <syamlalviswanath@gmail.com>
2018-08-11 15:32:39 +0530

    Update git_example.py

--- git_example.py
+++ git_example.py
@@ -11,8 +11,8 @@ def sigmoid(x):
   return (1 / (1 + numpy.exp(-x)))
 
 def tanh(x):
-  # enter code below
+  return  (1/(1+np.exp(-x)))
   
 def relu(x):
   # entr code below
-  
+  return  np.max(0,x)
